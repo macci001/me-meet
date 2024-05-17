9f08cac4cb89d7e48e7379585a2240f95babdd27
Maharshi <maharshialpesh@gmail.com>
2024-05-17 20:31:44 +0530

    Deprecating Version 1 of peer connection.

--- apps/web/app/vc/page.tsx
+++ apps/web/app/vc/page.tsx
@@ -1,14 +1,10 @@
-import Sender from "../../components/Sender";
-import Receiver from "../../components/Receiver";
+import Room from "../components/Room";
 
 const vc = () => {
     return <div>
-        <div className="w-[40vw] h-[23vw] m-10">
-            <Sender />
-        </div>
-        <div className="w-[40vw] h-[23vw] m-10">
-            <Receiver />
+        <div>
+            <Room />
         </div>
     </div>
-}
+};
 export default vc;
