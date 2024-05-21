8b9abe62688c23fd5a29b6fc4741d938bf01fa59
Maharshi <maharshialpesh@gmail.com>
2024-05-21 22:52:56 +0530

    Mirroring the video of sender.

--- apps/web/app/components/SenderComponent.tsx
+++ apps/web/app/components/SenderComponent.tsx
@@ -6,7 +6,7 @@ const SenderComponent = ({id} : {id: string}) => {
     const userName = session.data?.user?.name;
     return (<div className="fixed bottom-[8vh] right-[1vw]">
         <div className="absolute right-[1vh] top-[1vh] bg-fuchsia-200 p-0.5 rounded-md font-medium opacity-75 text-fucnsia-400">{userName}</div>
-        <video className="w-[30vmin] h-[20vmax] md:w-[30vmax] md:h-[15vmax] 2xl:w[14-vmax] 2xl:h-[7-vmax] bg-fuchsia-100  rounded-[1vw] shadow-inner" id={id} key={"sender"}>Sender</video>
+        <video className="w-[30vmin] h-[20vmax] md:w-[30vmax] md:h-[15vmax] 2xl:w[14-vmax] 2xl:h-[7-vmax] bg-fuchsia-100  rounded-[1vw] shadow-inner" id={id} key={"sender"} style={{transform: "rotateY(180deg)"}}>Sender</video>
     </div>)
 }
 export default SenderComponent;
